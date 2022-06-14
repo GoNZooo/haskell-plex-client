825ec4fbaef624ff5fd04fd82c8f650919269705
Rickard Andersson <gonz@severnatazvezda.com>
2022-06-14 11:02:43 +0300

    feat: add plex token & IP to types
    Will be basis for being able to execute API calls, etc.

--- src/Run.hs
+++ src/Run.hs
@@ -9,12 +9,16 @@ run options = do
   loadDotEnvFile $ options ^. optionsEnvironmentFile
   lo <- logOptionsHandle stderr (options ^. optionsVerbose)
   pc <- mkDefaultProcessContext
+  plexToken <- readEnvironmentVariable "PLEX_TOKEN"
+  plexIp <- readEnvironmentVariable "PLEX_IP"
   withLogFunc lo $ \lf ->
     let app =
           App
             { _appLogFunc = lf,
               _appProcessContext = pc,
-              _appOptions = options
+              _appOptions = options,
+              _appPlexToken = plexToken,
+              _appPlexIp = plexIp
             }
      in runRIO app runApp
 
--- src/Types.hs
+++ src/Types.hs
@@ -5,6 +5,24 @@ module Types where
 import Qtility
 import RIO.Process (HasProcessContext (..), ProcessContext)
 
+newtype PlexToken = PlexToken {unPlexToken :: ByteString}
+  deriving (Eq, Show, Read, IsString, Generic, FromEnvironmentValue)
+
+class HasPlexToken env where
+  plexTokenL :: Lens' env PlexToken
+
+instance HasPlexToken PlexToken where
+  plexTokenL = id
+
+newtype PlexIp = PlexIp {unPlexIp :: ByteString}
+  deriving (Eq, Show, Read, IsString, Generic, FromEnvironmentValue)
+
+class HasPlexIp env where
+  plexIpL :: Lens' env PlexIp
+
+instance HasPlexIp PlexIp where
+  plexIpL = id
+
 -- | Command line arguments
 data Options = Options
   { _optionsVerbose :: !Bool,
@@ -14,13 +32,23 @@ data Options = Options
 data App = App
   { _appLogFunc :: !LogFunc,
     _appProcessContext :: !ProcessContext,
-    _appOptions :: !Options
+    _appOptions :: !Options,
+    _appPlexToken :: !PlexToken,
+    _appPlexIp :: !PlexIp
   }
 
 foldMapM makeLenses [''Options, ''App]
+
+foldMapM makeWrapped [''PlexToken, ''PlexIp]
 
 instance HasLogFunc App where
   logFuncL = appLogFunc
 
 instance HasProcessContext App where
   processContextL = appProcessContext
+
+instance HasPlexToken App where
+  plexTokenL = appPlexToken
+
+instance HasPlexIp App where
+  plexIpL = appPlexIp
